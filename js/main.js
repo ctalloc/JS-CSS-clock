a2b56d8b2419e9f567bf768f685f4dac8d46206a
ctalloc <cecilia.tallo@gmail.com>
2021-04-15 17:11:12 +0200

    First commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const secHand = document.querySelector('.sec-hand');
+const minHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
+
+function setDate(){
+    const now = new Date;
+    const seconds = now.getSeconds();
+    const secondsDegrees = ((seconds/60) * 360) +90;
+    secHand.style.transform = `rotate(${secondsDegrees}deg)`;
+    const mins = now.getMinutes();
+    const minutesDegrees = ((mins/60)*360) + 90;
+    minHand.style.transform = `rotate(${minutesDegrees}deg)`;
+    const hours = now.getHours();
+    const hourDegrees = ((hours/12)*360) + 90;
+    hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+}
+
+setInterval(setDate,1000)
